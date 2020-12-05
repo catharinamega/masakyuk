8e949025d4d2ed21c2a6dee82ed4190da633aac5
catharinamega <67580484+catharinamega@users.noreply.github.com>
2020-12-05 17:04:55 +0700

    cuma ganti tabel satuan di model

--- app/Models/Resep.php
+++ app/Models/Resep.php
@@ -18,7 +18,7 @@ class Resep extends Model
     public function get_all($id_resep){
        
         $cmd = "SELECT gambar_resep, judul_resep, porsi, durasi, k.jenis_kategori, penjelasan_resep, CONCAT(rb.qty,' ',s.tipe_satuan,' ',b.nama_bahan) `bahan`, prosedur_resep ".
-                "FROM ".$this->tabel_resep." r, ".$this->tabel_resepbahan." rb, ".$this->tabel_kategori." k, ".$this->tabel_bahan." b, satuan s ".
+                "FROM ".$this->tabel_resep." r, ".$this->tabel_resepbahan." rb, ".$this->tabel_kategori." k, ".$this->tabel_bahan." b, ".$this->tabel_satuan." s ".
                 "WHERE r.id_resep=:id_resep AND k.id_kategori = r.id_kategori AND rb.id_resep = r.id_resep AND rb.id_bahan = b.id_bahan AND rb.id_satuan = s.id_satuan;";
         
         // $cmd2 = "SELECT rb.qty, s.tipe_satuan, b.nama_bahan ".
