79264837841865e1407119e5ac08012fbadcab0a
Eillen25 <57930067+Eillen25@users.noreply.github.com>
2020-12-07 23:30:56 +0700

    ketinggalan faq s&k

---
+++ resources/views/faq.blade.php
@@ -0,0 +1,38 @@
+@extends('shared/base2')
+
+@section('judul','Pertanyaan Umum')
+
+@section('isi_konten')
+
+    <div class="container">
+        <div style="margin-left:20px; text-align: left; padding-top:20px;">
+            <h1>Pertanyaan Umum</h1>
+        </div>
+        <div style="margin-top:30px;">
+            <div class="mt-3" style="margin-left:20px; text-align: left; outline; border: 1px solid #44628F; margin: auto; padding: 20px; border-radius: 12px; width: 1000px;">
+                <h4>Apa itu MasakYuk?</h4>
+                <p>MasakYuk adalah sebuah website e-commerce yang menyediakan bahan-bahan makanan beserta resep masakan untuk Anda yang ingin serba cepat.</p>
+            </div>
+
+            <div class="mt-3" style="margin-left:20px; text-align: left; outline; border: 1px solid #44628F; margin: auto; padding: 20px; border-radius: 12px; width: 1000px;">
+                <h4>Gimana Cara Bayarnya?</h4>
+                <p>Pembayaran bisa dilakukan dengan cara transfer bank ataupun bayar ditempat. Setelah melakukan pembayaran, kami akan memeriksa pembayaran Anda dan segera mengirim pesanan Anda.</p>
+            </div>
+
+            <div class="mt-3" style="margin-left:20px; text-align: left; outline; border: 1px solid #44628F; margin: auto; padding: 20px; border-radius: 12px; width: 1000px;">
+                <h4>Pengiriman berapa lama ya??</h4>
+                <p>Setelah pembayaran Anda kami periksa, kami akan segera mengirimkan bahan-bahan Anda tepat kedepan rumah.</p>
+            </div>
+
+            <div class="mt-3" style="margin-left:20px; text-align: left; outline; border: 1px solid #44628F; margin: auto; padding: 20px; border-radius: 12px; width: 1000px;">
+                <h4>Wajib bikin akun?</h4>
+                <p>Tidak, kok. Anda bisa melakukan pembelian tanpa mendaftar sebagai anggota. Namun, Anda bisa melakukan transaksi dengan lebih cepat jika Anda mendaftar.</p>
+            </div>
+        
+            <div class="mt-3 mb-5" style="margin-left:20px; text-align: left; outline; border: 1px solid #44628F; margin: auto; padding: 20px; border-radius: 12px; width: 1000px;">
+                <h4>Semua bahan selalu ready?</h4>
+                <p>Tidak semua bahan selalu siap di pusat kami, namun kami akan selalu menampilkan bahan-bahan yang tersedia.</p>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/sdank.blade.php
@@ -0,0 +1,38 @@
+@extends('shared/base2')
+
+@section('judul','Syarat & Ketentuan')
+
+@section('isi_konten')
+
+    <div class="container">
+        <div style="margin-left:20px; text-align: left; padding-top:20px;">
+            <h1>Syarat dan Ketentuan</h1>
+        </div>
+        <div style="margin-top:30px;">
+            <div style="margin-left:20px; text-align: left;">
+                <h4>A. Ketentuan Umum</h4>
+                <ul>
+                    <li>Dengan menggunakan, berbelanja dan/atau mendaftarkan diri Anda di masakyuk.com, berarti Anda setuju untuk terikat dan patuh pada syarat dan ketentuan yang berlaku.</li>
+                    <li>Syarat dan ketentuan ini dapat berubah sewaktu-waktu dan kami tidak berkewajiban untuk memberitahukannya kepada Anda.</li>
+                    <li>Syarat dan ketentuan ini kami buat untuk kepentingan bersama, untuk menjaga hak dan kewajiban masing-masing pihak, dan tidak dimaksudkan untuk merugikan salah satu pihak.</li>
+                </ul>
+            </div>
+            <div style="margin-left:20px; text-align: left;">
+                <h4>B. Informasi Produk</h4>
+                <ul>
+                    <li>Dengan melakukan transaksi pemesanan secara online di masakyuk.com, Anda kami anggap telah mengerti informasi produk yang akan Anda beli.</li>
+                    <li>Produk yang tersedia di masakyuk.com sesuai dengan katalog online. Kami berusaha menyajikan data seakurat mungkin tanpa rekayasa agar Anda selaku pembeli tidak dirugikan.</li>
+                    <li>Harga produk dalam situs ini adalah benar pada saat dicantumkan. Harga yang tercantum adalah harga produk semata, tidak termasuk ongkos kirim. Ongkos kirim dihitung otomatis (berdasarkan harga dari jasa ekspedisi) sesuai dengan alamat pengiriman yang Anda berikan pada saat transaksi pemesanan.</li>
+                </ul>
+            </div>
+            <div style="margin-left:20px; text-align: left; margin-bottom:50px;">
+                <h4>C. Pembayaran</h4>
+                <ul>
+                    <li>Mata uang yang dipakai untuk pembayaran adalah Rupiah (IDR). Pembayaran bisa melalui OVO, Gopay, ATM, internet banking, mobile banking, transfer antarbank ke rekening bank yang telah kami informasikan kepada Anda.</li>
+                    <li>Pembayaran dianggap lunas jika uang telah kami terima sesuai dengan jumlah yang harus dibayarkan.</li>
+                    <li>Keterlambatan proses transfer antarbank bukan tanggung jawab kami.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+@endsection
