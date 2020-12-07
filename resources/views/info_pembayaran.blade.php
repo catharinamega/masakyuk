36cad1ed9ad3c5dea085caf3ba10abc1ff5335a2
Eillen25 <57930067+Eillen25@users.noreply.github.com>
2020-12-07 23:29:55 +0700

    faq, s&k, info_pembayaran_pengiriman

---
+++ resources/views/info_pembayaran.blade.php
@@ -0,0 +1,83 @@
+@extends('shared/base2')
+
+@section('judul','Info Pembayaran')
+
+@section('isi_konten')
+
+<div class="container">
+    <div style="margin-left:20px; text-align: left; padding-top:20px;">
+        <h1>Info Pembayaran</h1>
+    </div>
+    <div style="margin-top:30px;">
+        <div style="margin-left:20px; text-align: left;">
+            <h5>Pembayaran produk-produk MasakYuk dapat dilakukan melalui 4 media, yaitu:</h5>
+            <div class="row">
+                <table class="table table-borderless">
+
+                    <tbody>
+                        <tr>
+                            <td style='width: 30%;'>
+                                <div>
+                                    <img src="assets/img/ovo.png" alt="ovo.png" style="width:200px;">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="container" style="text-align:left; padding-top:20px;">
+                                    <h5>OVO</h5>
+                                    <p style="margin-bottom:0px;">a/n Denisa Flo</p>
+                                    <p>0812-1212-1212</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style='width: 30%;'>
+                                <div>
+                                    <img src="assets/img/gopay.png" alt="gopay.png" style="width:200px;">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="container" style="text-align:left; padding-top:20px;">
+                                    <h5>Gopay</h5>
+                                    <p style="margin-bottom:0px;">a/n Denisa Flo</p>
+                                    <p>0812-1212-1212</p> 
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style='width: 30%;'>
+                                <div>
+                                    <img src="assets/img/mandiri.png" alt="mandiri.png" style="width:200px;">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="container" style="text-align:left; padding-top:20px;">
+                                    <h5>Mandiri</h5>
+                                    <p style="margin-bottom:0px;">a/n Denisa Flo</p>
+                                    <p>9000.0000.000</p> 
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style='width: 30%;'>
+                                <div>
+                                    <img src="assets/img/bca.png" alt="bca.png" style="width:200px;">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="container" style="text-align:left; padding-top:20px;">
+                                    <h5>BCA</h5>
+                                    <p style="margin-bottom:0px;">a/n Denisa Flo</p>
+                                    <p>9000.0000.000</p> 
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div style="margin-left:20px; text-align: left; margin-bottom:50px;">
+        <p>Seluruh pembayaran akan diperiksa secara otomatis oleh MasakYuk dan pesanan akan segera dikirim setelah pembayaran dinyatakan valid.</p>
+    </div>
+</div>
+@endsection
---
+++ resources/views/info_pengiriman.blade.php
@@ -0,0 +1,53 @@
+@extends('shared/base2')
+
+@section('judul','Info Pengiriman')
+
+@section('isi_konten')
+
+<div class="container">
+    <div style="margin-left:20px; text-align: left; padding-top:20px;">
+        <h1>Info Pengiriman</h1>
+    </div>
+    <div style="margin-top:30px;">
+        <div style="margin-left:20px; text-align: left;">
+            <h5>Pengiriman produk-produk MasakYuk dapat dilakukan dengan 2 cara, yaitu:</h5>
+            <div class="row" style="margin-bottom:50px;">
+                <table class="table table-borderless">
+
+                    <tbody>
+                        <tr>
+                            <td style='width: 20%;'>
+                                <div>
+                                    <img src="assets/img/gosend.png" alt="gosend.png" style="width:120px;">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="container" style="text-align:left; padding-top:20px;">
+                                    <h5>Gosend</h5>
+                                    <p>MasakYuk akan mengirimkan produk anda dengan Gosend dan kemudian mengakumulasi harga pengiriman dengan total pembelian.</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style='width: 20%;'>
+                                <div>
+                                    <img src="assets/img/grab.png" alt="grab.png" style="width:120px;">
+                                </div>
+                            </td>
+                            <td>
+                                <div class="container" style="text-align:left; padding-top:20px;">
+                                    <h5>GrabExpress</h5>
+                                    <p>MasakYuk akan mengirimkan produk anda dengan GrabExpress dan kemudian mengakumulasi harga pengiriman dengan total pembelian.</p>
+                                </div>
+                            </td>
+                        </tr>
+                        
+                    </tbody>
+                </table>
+            </div>
+            
+        </div>
+
+    </div>
+</div>
+@endsection
