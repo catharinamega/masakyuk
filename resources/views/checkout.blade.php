85ec7ac0210046d8844221b7acbfacdb5ab68063
Eillen25 <57930067+Eillen25@users.noreply.github.com>
2020-12-11 13:55:16 +0700

    checkout pembayaran

---
+++ resources/views/checkout.blade.php
@@ -0,0 +1,179 @@
+@extends('shared/base2')
+
+@section('judul','Checkout')
+
+@section('isi_konten')
+
+<div class="container">
+    <div style="margin-left:-55px; text-align: left; padding-top:20px;">
+        <table class="table table-borderless" style="height:50px;">
+            <tbody>
+                <tr>
+                    <td style='width: 5%; padding-top:8px; padding-left:0px;'>
+                        <div>
+                            <a href="/" style="color:black;">
+                                <i class="fa fa-arrow-left" aria-hidden="true" style="width:40px; height:40px;"></i>
+                            </a>
+                        </div>
+                    </td>
+                    <td style="padding:0px;">
+                        <div class="container" style="text-align:left;">
+                            <h1>Checkout</h1>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div style="margin-top:10px;">
+        <div style="margin-left:20px; text-align: left;">
+            <h5>Alamat Pengiriman</h5>
+            <div style="text-align:left; background-color: #E9F3F5; width: 600px; border: 2px solid #C5E0EF; padding: 10px; margin-bottom:15px;">
+                <p style="margin-bottom:5px;">Nina</p>
+                <p style="margin-bottom:5px;">ahahhah</p>
+                <p style="margin-bottom:5px;">jvbwuevbwe</p>
+            </div>
+            <div style="text-align:left; margin-bottom:50px;">
+                <button type="submit" class="btn btn-light" style="background-color:#B2DEF0; border: 2px solid #C5E0EF; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);">Pilih Alamat Lain</button>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-12 row mt-4">
+        <div class="col-md-5" style="text-align: center;"><h5>Bahan</h5></div>
+        <div class="col-md-2" style="text-align: center;"><h5>Harga</h5></div>
+        <div class="col-md-2" style="text-align: center;"><h5>Jumlah</h5></div>
+        <div class="col-md-2" style="text-align: center;"><h5>Total</h5></div>
+    </div>
+
+    <div class="col-md-12 row mt-4">
+        <div class="col-md-5" style="text-align: left;">Nama Bahan</div>
+        <div class="col-md-2 row" >
+            <div class=col-md-3>Rp</div>
+            <div class=col-md-9 style="text-align:right;">10.000</div>
+        </div>
+        <div class="col-md-2" style="text-align: center; padding-left:80px;">8</div>
+        <div class="col-md-2 row" style="margin-left:20px;">
+            <div class=col-md-3>Rp</div>
+            <div class=col-md-9 style="text-align:right; ">80.000</div>
+        </div>
+    </div>
+    <div class="col-md-12 row mt-4">
+        <div class="col-md-5" style="text-align: left;">Nama Bahan</div>
+        <div class="col-md-2 row" >
+            <div class=col-md-3>Rp</div>
+            <div class=col-md-9 style="text-align:right;">10.000</div>
+        </div>
+        <div class="col-md-2" style="text-align: center; padding-left:80px;">8</div>
+        <div class="col-md-2 row" style="margin-left:20px;">
+            <div class=col-md-3>Rp</div>
+            <div class=col-md-9 style="text-align:right; ">80.000</div>
+        </div>
+    </div>
+    <div class="col-md-12 row mt-4" style="margin-bottom:60px;">
+        <div class="col-md-5" style="text-align: left;">Nama Bahan</div>
+        <div class="col-md-2 row" >
+            <div class=col-md-3>Rp</div>
+            <div class=col-md-9 style="text-align:right;">10.000</div>
+        </div>
+        <div class="col-md-2" style="text-align: center; padding-left:80px;">8</div>
+        <div class="col-md-2 row" style="margin-left:20px;">
+            <div class=col-md-3>Rp</div>
+            <div class=col-md-9 style="text-align:right; ">80.000</div>
+        </div>
+    </div>
+
+    <div class="col-md-12 row" style="margin-left:0px;">
+        <div><h6>Opsi Pengiriman:</h6></div>
+        <div class="btn-group" style="margin-left: 38px;">
+            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Pilih Opsi Pengiriman
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">Gosend</a>
+                <a class="dropdown-item" href="#">GrabExpress</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-12 row" style="margin-left:0px; margin-top:30px; margin-bottom:30px;">
+        <div><h6>Opsi Pembayaran:</h6></div>
+        <div class="btn-group" style="margin-left: 30px;">
+            <button type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Pilih Opsi Pembayaran
+            </button>
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="#">BCA</a>
+                <a class="dropdown-item" href="#">Mandiri</a>
+                <a class="dropdown-item" href="#">OVO</a>
+                <a class="dropdown-item" href="#">Gopay</a>
+            </div>
+        </div>
+    </div>
+
+
+    <div style="margin-top:10px;">
+        <div class="d-flex justify-content-end" style="margin-left:20px; text-align: center;">
+            <div style="text-align:left; width: 400px; border: 2px solid #000000; padding: 15px; margin-bottom:15px;">
+                <table class="table table-borderless" style="height:20px; margin-bottom:0px;">
+                    <tbody>
+                        <tr>
+                            <td style="width: 50%; padding:4px;">
+                                <div>
+                                    <p style="margin-bottom:8px; text-align:left;">Total Harga</p>
+                                </div>
+                            </td>
+                            <td style="padding:4px;">
+                                <div class="container" style="text-align:right;">
+                                    <p style="margin-bottom:8px;">: Rp</p>
+                                </div>
+                            </td>
+                            <td style="padding:4px;">
+                                <div class="container" style="text-align:right;">
+                                    <p style="margin-bottom:8px;">90.000</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%; padding:4px;">
+                                <div>
+                                    <p style="margin-bottom:8px; text-align:left;">Total Ongkos Kirim</p>
+                                </div>
+                            </td>
+                            <td style="padding:4px;">
+                                <div class="container" style="text-align:right;">
+                                    <p style="margin-bottom:8px;">: Rp</p>
+                                </div>
+                            </td>
+                            <td style="padding:4px;">
+                                <div class="container" style="text-align:right;">
+                                    <p style="margin-bottom:8px;">90.000</p>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="width: 50%; padding:4px;">
+                                <div>
+                                    <p style="margin-bottom:8px; text-align:left;"><strong>Total Harga</strong></p>
+                                </div>
+                            </td>
+                            <td style="padding:4px;">
+                                <div class="container" style="text-align:right;">
+                                    <p style="margin-bottom:8px;"><strong>: Rp</strong></p>
+                                </div>
+                            </td>
+                            <td style="padding:4px;">
+                                <div class="container" style="text-align:right;">
+                                    <p style="margin-bottom:8px;"><strong>90.000</strong></p>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="d-flex justify-content-end" style="text-align:left; margin-bottom:50px; margin-top:30px;">
+            <button type="submit" class="btn btn-light" style="background-color:#B2DEF0; width:200px; border: 2px solid #C5E0EF; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"><strong>Bayar</strong></button>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/pembayaran.blade.php
@@ -0,0 +1,237 @@
+@extends('shared/base2')
+
+@section('judul','Pembayaran')
+
+@section('isi_konten')
+
+<div class="container">
+    <div style="margin-left:-55px; text-align: left; padding-top:20px;">
+        <table class="table table-borderless" style="height:50px;">
+            <tbody>
+                <tr>
+                    <td style='width: 5%; padding-top:8px; padding-left:0px;'>
+                        <div>
+                            <a href="/" style="color:black;">
+                                <i class="fa fa-arrow-left" aria-hidden="true" style="width:40px; height:40px;"></i>
+                            </a>
+                        </div>
+                    </td>
+                    <td style="padding:0px;">
+                        <div class="container" style="text-align:left;">
+                            <h1>Pembayaran</h1>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <div style="margin-top:10px;">
+        <div style="margin-left:20px; text-align: left;">
+            <h5>Total Pembayaran: Rp 90.000</h5>
+            <hr style="width:50%;text-align:left;margin-left:0; border-top: 1px solid black;">
+            <p>Bank BCA</p>
+            <p style="margin-bottom:0px;">Nomor Rekening:</p>
+            <input type="number" value="900012345" id="norek" style="width:150px;">
+            <button class="btn btn-link" style="width:80px;" onclick="myFunction()">Salin</button>
+
+            <hr style="text-align:left;margin-left:0;border-top: 2px solid black;">
+
+
+<!-- Petunjuk BCA -->
+            <div id="accordion">
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapseBCA1">
+                            Petunjuk Transfer mBanking
+                        </a>
+                    </div>
+                    <div id="collapseBCA1" class="collapse show" data-parent="#accordion"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Pilih m-Transfer > rekening BCA </li>
+                                <li>Masukkan nomor rekening 124 6786 5674 4321 dan pilih Send. </li>
+                                <li>Periksa informasi yang tertera di layar. Pastikan nama penerima adalah Denisa dan total tagihan sudah benar. Jika benar pilih Ya. </li>
+                                <li>Masukkan PIN m-BCA Anda dan pilih OK </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapseBCA2">
+                            Petunjuk Transfer iBanking
+                        </a>
+                    </div>
+                    <div id="collapseBCA2" class="collapse" data-parent="#accordion"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Pilih Transfer Dana > rekening BCA</li>
+                                <li>Masukkan nomor rekening 124 6786 5674 4321 dan pilih Lanjutkan</li>
+                                <li>Periksa informasi yang tertera di layar. Pastikan nama penerima adalah Denisa dan total tagihan sudah benar. Jika benar pilih Ya.</li>
+                                <li>Masukkan respon KeyBCA Anda dan klik Kirim</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapseBCA3">
+                            Petunjuk Transfer ATM
+                        </a>
+                    </div>
+                    <div id="collapseBCA3" class="collapse" data-parent="#accordion"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Pilih Transaksi Lainnya > Transfer > Ke Rek BCA </li>
+                                <li>Masukkan nomor rekening 124 6786 5674 4321 dan pilih Benar.</li>
+                                <li>Periksa informasi yang tertera di layar. Pastikan nama yang tertera adalah Denisa dan total tagihan sudah benar. Jika benar pilih Ya</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+<br><br>
+
+
+<!-- Petunjuk Mandiri -->
+            <div id="accordionM">
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapseOVO1">
+                            Petunjuk Transfer mBanking
+                        </a>
+                    </div>
+                    <div id="collapseOVO1" class="collapse show" data-parent="#accordionM"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Pilih Bayar/Beli</li>
+                                <li>Pilih m-Transfer > rekening Mandiri</li>
+                                <li>Pilih m-Transfer > rekening Mandiri dan pilih Benar</li>
+                                <li>Periksa informasi yang tertera di layar. Pastikan nama yang tertera adalah Denisa
+                                    dan total tagihan sudah benar. Jika benar, tekan angka 1 dan pilih Ya.</li>
+                                <li>Periksa layar konfirmasi dan pilih Ya</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapseOVO2">
+                            Petunjuk Transfer iBanking
+                        </a>
+                    </div>
+                    <div id="collapseOVO2" class="collapse" data-parent="#accordionM"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Pilih Transfer Dana > rekening Mandiri</li>
+                                <li>Masukkan nomor rekening 124 6786 5674 4321 dan pilih Lanjutkan.</li>
+                                <li>Periksa informasi yang tertera di layar. Pastikan nama penerima adalah Denisa dan
+                                    total tagihan sudah benar. Jika benar pilih Ya.</li>
+                                <li>Masukkan pin token anda dan klik Kirim</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapseOVO3">
+                            Petunjuk Transfer ATM
+                        </a>
+                    </div>
+                    <div id="collapseOVO3" class="collapse" data-parent="#accordionM"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Pilih Transaksi Lainnya > Transfer > Ke Rek Mandiri </li>
+                                <li>Masukkan nomor rekening 124 6786 5674 4321 dan pilih Benar.</li>
+                                <li>Periksa informasi yang tertera di layar. Pastikan nama yang tertera adalah Denisa
+                                    dan total tagihan sudah benar. Jika benar pilih Ya</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+<br><br>
+
+<!-- Petunjuk Gopay -->
+            <div id="accordionG">
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapsegopay">
+                            Petunjuk Pembayaran dengan Gopay
+                        </a>
+                    </div>
+                    <div id="collapsegopay" class="collapse show" data-parent="#accordionG"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Bukalah aplikasi Gojek</li>
+                                <li>Pada menu utama silahkan tekan Bayar. Akan muncul tampilan menu Transfer</li>
+                                <li>Pada pojok kiri bawah pilih Nomor HP. Akan muncul menu Bayar lewat nomor HP</li>
+                                <li>Silahkan masukkan nomor 0856924145563 atas nama Mega</li>
+                                <li>Masukkan nominal yang akan anda transfer dan tekan konfirmasi</li>
+                                <li>Tekan Bayar</li>
+                                <li>Masukkan Pin Gopay</li>
+                                <li>Pembayaran berhasil dilakukan</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+<br><br>
+
+<!-- Petunjuk OVO -->
+<div id="accordionO">
+                <div style="text-align:left; width:800px;">
+                    <div class="card-header" style="text-align:left; width:700px; background-color:#E9EAEB;">
+                        <a class="card-link" data-toggle="collapse" href="#collapseOvo">
+                            Petunjuk Pembayaran dengan OVO
+                        </a>
+                    </div>
+                    <div id="collapseOvo" class="collapse show" data-parent="#accordionO"
+                        style="text-align:left; width:700px;">
+                        <div class="card-body" style="text-align:left; width:700px;">
+                            <ol>
+                                <li>Bukalah aplikasi OVO</li>
+                                <li>Klik menu Transfer yang ada di halaman depan</li>
+                                <li>Masukkan nominal yang ingin ditransfer. (Diinformasikan bahwa minimal pembayaran adalah Rp10.000)</li>
+                                <li>Pilih transfer Antar OVO</li>
+                                <li>Masukkan nomor ponsel tujuan transfer 085692414563 atas nama Mega</li>
+                                <li>Isi formulir transfer dengan baik dan benar</li>
+                                <li>Klik Transfer</li>
+                                <li>Cek informasi yang ditampilkan apakah sudah sesuai, kemudian klik Konfirmasi</li>
+                                <li>Masukkan kode keamanan atau security code akun OVO Anda</li>
+                                <li>Akan muncul pemberitahuan di layar apabila transfer berhasil dilakukan</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-end" style="text-align:left; margin-bottom:50px; margin-top:30px;">
+                <button type="submit" class="btn btn-light" style="background-color:#B2DEF0; width:200px; border: 2px solid #C5E0EF; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);"><strong>Oke</strong></button>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+
+<script>
+function myFunction() {
+  var copyText = document.getElementById("norek");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+//   alert("Copied the text: " + copyText.value);
+}
+</script>
+@endsection
